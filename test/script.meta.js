a3d34e2bd7213358c6581973b125f54f53251c8d
JanielRosario <jkirarosario@gmail.com>
2026-04-14 16:53:22 -0400

    Update URLs with new GitHub token

--- test/script.meta.js
+++ test/script.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         GIA Test Script
 // @version      1.0.0
-// @updateURL    https://oauth2:YOUR_TOKEN@raw.githubusercontent.com/ugomez809/GIA-TamperMonkey/main/test/script.meta.js
-// @downloadURL  https://oauth2:YOUR_TOKEN@raw.githubusercontent.com/ugomez809/GIA-TamperMonkey/main/test/script.user.js
+// @updateURL    https://oauth2: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@raw.githubusercontent.com/ugomez809/GIA-TamperMonkey/main/test/script.meta.js
+// @downloadURL  https://oauth2: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@raw.githubusercontent.com/ugomez809/GIA-TamperMonkey/main/test/script.user.js
 // ==/UserScript==
