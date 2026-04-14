8f915ac5df1c4840c23089979cca2ab7f775ee4f
JanielRosario <jkirarosario@gmail.com>
2026-04-14 16:52:53 -0400

    Add metadata for GIA Test Script

--- test/script.meta.js
+++ test/script.meta.js
@@ -1,1 +1,6 @@
-
+// ==UserScript==
+// @name         GIA Test Script
+// @version      1.0.0
+// @updateURL    https://oauth2:YOUR_TOKEN@raw.githubusercontent.com/ugomez809/GIA-TamperMonkey/main/test/script.meta.js
+// @downloadURL  https://oauth2:YOUR_TOKEN@raw.githubusercontent.com/ugomez809/GIA-TamperMonkey/main/test/script.user.js
+// ==/UserScript==
